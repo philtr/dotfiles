090860cf7f5c7cb86f1760efbe67e793511a7a46
Phillip Ridlen <phillip@ovenbits.com>
2013-05-25 21:21:34 -0500

    Add some fancy to the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+echo '
+                  _      _
+      |          | | o  | |
+    __|   __ _|_ | |    | |  _   ,
+   /  |  /  \_|  |/  |  |/  |/  / \_
+   \_/|_/\__/ |_/|__/|_/|__/|__/ \/
+                 |\  P H I L T R
+                 |/
+
+Setting up dotfiles. If the ~/.dotfiles folder does not exist,
+it will be cloned down from http://git.spt.la/dotfiles. These
+dotfiles use ZShell and the Prezto framework, so if either of
+those are not yet set up properly, they will be configured
+at this time.
+'
+
 # Setup
 if [[ -z $HOMEDIR ]]
 then
@@ -13,16 +29,21 @@ fi
 
 if [[ ! -d $DOTS ]]
 then
-  echo "Cloning dotfiles"
+  echo "Cloning dotfiles.."
   mkdir -p $DOTS
   git clone --recursive git@bitbucket.org:philtr/dotfiles.git "$DOTS"
 fi
 
 
 # First install Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOMEDIR/.zprezto"
+if [[ ! -d $HOMEDIR/.zprezto ]]
+then
+  echo "Installing Prezto (https://github.com/sorin-ionescu/prezto)..."
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOMEDIR/.zprezto"
+fi
 
 # Link zsh configuration files
+echo "Linking ZSH configuration files..."
 ln -s $DOTS/zsh/zlogin            $HOMEDIR/.zlogin
 ln -s $DOTS/zsh/zlogout           $HOMEDIR/.zlogout
 ln -s $DOTS/zsh/zpreztorc         $HOMEDIR/.zpreztorc
@@ -31,27 +52,27 @@ ln -s $DOTS/zsh/zshenv            $HOMEDIR/.zshenv
 ln -s $DOTS/zsh/zshrc             $HOMEDIR/.zshrc
 
 # Link git configuration files
+echo "Linking Git configuration files..."
 ln -s $DOTS/git/gitconfig         $HOMEDIR/.gitconfig
 ln -s $DOTS/git/gitignore         $HOMEDIR/.gitignore
 
 # Link ruby configuration
+echo "Linking Ruby-related configurations..."
 mkdir -p $HOMEDIR/.bundle
 ln -s $DOTS/ruby/bundler          $HOMEDIR/.bundle/config
 ln -s $DOTS/ruby/gemrc            $HOMEDIR/.gemrc
 ln -s $DOTS/ruby/pryrc            $HOMEDIR/.pryrc
+ln -s $DOTS/pow/powconfig         $HOMEDIR/.powrc
+ln -s $DOTS/middleman             $HOMEDIR/.middleman
 
 # Link vim configuration
+echo "Linking vim configuration files"
+ln -s $DOTS/vim                   $HOMEDIR/.vim
 ln -s $DOTS/vim/settings/vimrc    $HOMEDIR/.vimrc
 ln -s $DOTS/vim/setting/gvimrc    $HOMEDIR/.gvimrc
 
-# Link pow configuration
-ln -s $DOTS/pow/powconfig         $HOMEDIR/.powrc
-
-# Link middleman configuration
-ln -s $DOTS/middleman             $HOMEDIR/.middleman
-
 # Print Manual Instructions
-echo "Further Instructions:\n"
+echo "\n\nATTENTION! Further Instructions:\n"
 
 # iTerm2
 echo "  * Set iTerm configuration to load config"
