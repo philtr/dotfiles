123a9f9a1bb06b7b7b8587abb3fbb0caea77609c
Phillip Ridlen <phillip@ovenbits.com>
2015-02-02 09:19:28 -0600

    Update things.

--- zsh/components/zshrc/ruby.zsh
+++ zsh/components/zshrc/ruby.zsh
@@ -1,7 +1,7 @@
 # Rails
 alias b='bundle exec'
 alias c="cane --abc-glob '{app,lib,test}/**/*.rb' --abc-max 15 --style-glob '{app,lib}/**/*.rb' --style-measure 100 --no-doc"
-alias migrate="rake db:migrate"
+alias migrate="rake db:migrate db:test:prepare"
 alias rollback="rake db:rollback"
 alias t="turn -I\"lib:test\""
 
