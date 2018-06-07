e1813faf37c3997d0fcebf035385e15917f9fbc7
Phillip Ridlen <p@rdln.net>
2018-06-07 11:10:36 -0500

    Show dirty status for git repo

--- config/zsh/themes/super_basic.zsh
+++ config/zsh/themes/super_basic.zsh
@@ -1,11 +1,24 @@
 autoload -U colors; colors
 
 precmd() {
+  # show right prompt for git repos
   git status >& /dev/null
   if [ $? -eq 0 ]; then
+
+    local middle
     local revision="$(git rev-parse --short HEAD)"
     local branch="$(git rev-parse --abbrev-ref HEAD)"
-    export RPROMPT="%{$fg_bold[black]%}$branch%{%f%} %{$fg_bold[green]%}•%{%f%} $revision"
+
+    if [ -n "$(git status --porcelain)" ]; then
+      middle="%{$fg[red]%}✕%{%f%}"
+    else
+      middle="%{$fg_bold[green]%}•%{%f%}"
+    fi
+
+    export RPROMPT="%{$fg_bold[black]%}$branch%{%f%} $middle $revision"
+  else
+    # clear the right prompt
+    export RPROMPT=""
   fi
 
   export PROMPT="%~ %F{green}%%%f "
