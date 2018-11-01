bcf482247eccadee2856493e9d2ab0341ab50782
Phillip Ridlen <p@rdln.net>
2018-11-01 14:58:50 -0500

    "Always be extending the CLI" --@pengwynn

--- config/zsh/components/zshrc/git.zsh
+++ config/zsh/components/zshrc/git.zsh
@@ -6,9 +6,3 @@ alias gd="git difftool"
 alias gglg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 alias ggpush="git push origin \`gcb\`"
 alias gst="git status -sb"
-
-# new empty branch
-function gneb {
-  git checkout --orphan $1
-  git reset --hard
-}
