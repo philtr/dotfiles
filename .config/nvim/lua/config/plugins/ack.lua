83f9767627347d6462e2d9da917095d4e0d369d8
Phillip Ridlen <p@rdln.net>
2022-12-30 17:42:39 -0600

    Load :Ack results in :Trouble

--- .config/nvim/lua/config/plugins/ack.lua
+++ .config/nvim/lua/config/plugins/ack.lua
@@ -1,13 +1,21 @@
-local M = { "mileszs/ack.vim" }
+local M = {
+  "mileszs/ack.vim",
+
+  dependencies = {
+    "trouble.nvim",
+  },
+}
 
 function M.init()
-  vim.cmd [[
-    if executable("rg")
-      let g:ackprg = 'rg --vimgrep --no-heading --no-messages'
-    elseif executable("ag")
-      let g:ackprg = 'ag --vimgrep'
-    endif
-  ]]
+  -- Open the quickfix window with trouble.nvim
+  vim.g.ack_qhandler = "Trouble quickfix"
+
+  -- If they are installed, use ripgrep (rg) or the_silver_seearcher (ag), otherwise just use the default
+  if vim.fn.executable "rg" == 1 then
+    vim.g.ackprg = "rg --vimgrep --no-heading --no-messages"
+  elseif vim.fn.executable "ag" == 1 then
+    vim.g.ackprg = "ag --vimgrep"
+  end
 end
 
 return M
