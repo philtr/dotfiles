33fe68c6afb01e6dd370daaa4e5bd326f50287f1
Phillip Ridlen <p@rdln.net>
2023-01-05 22:23:29 -0600

    Move vim-tmux-navigator to config.plugins init

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -1,3 +1,6 @@
 return {
-  -- lazy.nvim will merge any lua file in config/plugins/*.lua into the main plugin spec
+  -- In addition to the plugin specs below, lazy.nvim will merge any lua file in
+  -- config/plugins/*.lua into the main plugin spec
+
+  "christoomey/vim-tmux-navigator",
 }
