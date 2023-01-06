6d1b95f5c70bdc085f916d1516e784321fdb190d
Phillip Ridlen <p@rdln.net>
2023-01-06 00:14:44 -0600

    Add "Lush" as an optional colorscheme

--- .config/nvim/lua/config/plugins/colors.lua
+++ .config/nvim/lua/config/plugins/colors.lua
@@ -1,23 +1,37 @@
-local M = {
+local tokyonight = {
   "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 999,
+
+  config = function()
+    local tokyonight = require "tokyonight"
+
+    tokyonight.setup {
+      style = "moon",
+
+      styles = {
+        floats = "dark",
+      },
+
+      dim_inactive = false,
+
+      on_highlights = function(hl, c)
+        hl.Floaterm = { bg = c.bg_dark }
+        hl.FloatermBorder = { bg = c.bg_dark, fg = c.cyan }
+      end,
+    }
+
+    tokyonight.load()
+  end,
+}
+
+local lush = {
+  "rktjmp/lush.nvim",
   lazy = false,
   priority = 999,
 }
 
-function M.config()
-  local tokyonight = require "tokyonight"
-
-  tokyonight.setup {
-    style = "moon",
-
-    styles = {
-      floats = "dark",
-    },
-
-    dim_inactive = false,
-  }
-
-  tokyonight.load()
-end
-
-return M
+return {
+  tokyonight,
+  lush,
+}
