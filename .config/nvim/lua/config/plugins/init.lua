0c424905ded72d7c2d26dbbb21e7e647ae372334
Phillip Ridlen <p@rdln.net>
2023-01-05 22:49:25 -0600

    Add neodev for LSP for neovim api

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -2,5 +2,9 @@ return {
   -- In addition to the plugin specs below, lazy.nvim will merge any lua file in
   -- config/plugins/*.lua into the main plugin spec
 
+  -- Seamless navigation between tmux panes and vim splits
   "christoomey/vim-tmux-navigator",
+
+  -- Neovim setup with full signature help, docs and completion for the nvim lua API.
+  { "folke/neodev.nvim", config = true },
 }
