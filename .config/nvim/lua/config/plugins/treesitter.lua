deb5a256d6a4de21920ce5c6829608ef36f5231d
Phillip Ridlen <p@rdln.net>
2023-01-05 23:39:17 -0600

    Update treesitter config
    - Add context plugin
    - Add vim-polyglot for now

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,23 +1,42 @@
-return {
+local M = {
   "nvim-treesitter/nvim-treesitter",
 
   build = ":TSUpdate",
   event = "BufReadPost",
 
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {
-        "eex",
-        "elixir",
-        "heex",
-        "javascript",
-        "ruby",
-      },
+  dependencies = {
+    -- display nested context at top of window
+    { "nvim-treesitter/nvim-treesitter-context", config = true },
 
-      auto_install = true,
-
-      highlight = { enable = true },
-      indent = { enable = true },
-    }
-  end,
+    -- until treesitter matures
+    "sheerun/vim-polyglot",
+  },
 }
+
+function M.config()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+      "elixir",
+      "javascript",
+      "ruby",
+    },
+
+    auto_install = true,
+
+    highlight = { enable = true },
+    indent = { enable = true },
+
+    additional_vim_regex_highlighting = true,
+  }
+
+  -- configure reformatting
+  vim.opt.formatoptions:remove "t"
+  vim.o.textwidth = 100
+
+  -- configure folding
+  vim.o.foldmethod = "expr"
+  vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+  vim.o.foldenable = false
+end
+
+return M
