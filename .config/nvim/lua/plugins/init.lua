3a1720feae55b4f42ed247fafad09e42e5f5e6ec
Phillip Ridlen <p@rdln.net>
2023-01-17 14:28:06 -0600

    Ensure tmux navigator bindings are set

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,5 +3,21 @@ return {
   -- plugins/*.lua into the main plugin spec
 
   -- Seamless navigation between tmux panes and vim splits
-  { "christoomey/vim-tmux-navigator", lazy = false },
+  {
+    "christoomey/vim-tmux-navigator",
+    
+    lazy = false,
+
+    init = function()
+      local bind = vim.keymap.set
+
+      bind({ "t", "n" }, "<C-h>", "<C-w><C-h>", { silent = true })
+      bind({ "t", "n" }, "<C-j>", "<C-w><C-j>", { silent = true })
+      bind({ "t", "n" }, "<C-k>", "<C-w><C-k>", { silent = true })
+      bind({ "t", "n" }, "<C-l>", "<C-w><C-l>", { silent = true })
+    end,
+  },
+
+  -- Neovim setup with full signature help, docs and completion for the nvim lua API.
+  { "folke/neodev.nvim", config = true },
 }
