18e84f6e88a516b8e9aa1880664d7988208de484
Phillip Ridlen <p@rdln.net>
2023-01-18 16:29:14 -0600

    Optionally load up a local config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,9 @@
+local load = require "config.load"
+
 require "config.options"
 require "config.lazy"
 
 require("config.keymap").setup()
+
+-- Load config/local.lua if it exists
+load "config.local"
---
+++ .config/nvim/lua/config/load.lua
@@ -0,0 +1,22 @@
+local is_available = function(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == "function" then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
+local load = function(name)
+  if is_available(name) then
+    require(name)
+  end
+end
+
+return load
