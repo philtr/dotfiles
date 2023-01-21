ab76d004b91f12eab6d34f9e12892a58f87b27eb
Phillip Ridlen <p@rdln.net>
2023-01-21 12:57:14 -0600

    Remove vim-polyglot again (for now)

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,31 +7,17 @@ local M = {
   dependencies = {
     -- display nested context at top of window
     { "nvim-treesitter/nvim-treesitter-context", config = true },
-
-    -- until treesitter matures
-    "sheerun/vim-polyglot",
   },
 }
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = {
-      "elixir",
-      "javascript",
-      "ruby",
-    },
-
     auto_install = true,
-
     highlight = { enable = true },
     indent = { enable = true },
 
     additional_vim_regex_highlighting = true,
   }
-
-  -- configure reformatting
-  vim.opt.formatoptions:remove "t"
-  vim.o.textwidth = 100
 
   -- configure folding
   vim.o.foldmethod = "expr"
