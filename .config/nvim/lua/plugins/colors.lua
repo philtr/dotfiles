91926fbe60fae58c68cbfce7215cdba3952bbd12
Phillip Ridlen <p@rdln.net>
2023-01-27 16:47:34 -0600

    Add more neovim color options

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,58 @@
+local base16 = {
+  "RRethy/nvim-base16",
+  enabled = false,
+  lazy = false,
+  priority = 999,
+
+  config = function()
+    vim.cmd [[
+      colorscheme base16-atelier-sulphurpool
+    ]]
+  end,
+}
+
+local calvera = {
+  "hoppercomplex/calvera-dark.nvim",
+  enabled = false,
+  lazy = false,
+
+  config = function()
+    vim.g.calvera_italic_comments = true
+    vim.g.calvera_italic_keywords = true
+    vim.g.calvera_italic_functions = true
+
+    require("calvera").set()
+  end,
+}
+
+local lush = {
+  "rktjmp/lush.nvim",
+  enabled = false,
+  lazy = false,
+  priority = 999,
+}
+
+local nord = {
+  "shaunsingh/nord.nvim",
+  enabled = false,
+  lazy = false,
+  priority = 999,
+
+  config = function()
+    vim.g.nord_borders = true
+    -- vim.g.nord_disable_background = true
+    -- vim.g.nord_enable_sidebar_background = true
+    vim.g.nord_italic = true
+    vim.g.nord_bold = true
+    vim.g.nord_uninform_diff_background = true
+
+    require("nord").set()
+  end,
+}
+
 local tokyonight = {
   "folke/tokyonight.nvim",
+  enabled = true,
   lazy = false,
   priority = 999,
 
@@ -25,13 +78,10 @@ local tokyonight = {
   end,
 }
 
-local lush = {
-  "rktjmp/lush.nvim",
-  lazy = false,
-  priority = 999,
-}
-
 return {
-  tokyonight,
+  base16,
+  calvera,
   lush,
+  nord,
+  tokyonight,
 }
