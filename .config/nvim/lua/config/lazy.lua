28b5b374d2ec1fb3b227b4e609bbf568f48125d3
Phillip Ridlen <p@rdln.net>
2023-05-08 09:26:09 -0500

    Disable Lazy.nvim auto-checker

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -28,7 +28,6 @@ require("lazy").setup {
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
