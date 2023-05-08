8c269d8265102ff3a30102e46ff6aa07a2328811
Phillip Ridlen <p@rdln.net>
2023-05-08 09:24:39 -0500

    Use Office Code Pro in Wezterm

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 
 return {
-  font_size = 13,
-  freetype_load_target = "Light",
+  font_size = 14,
+  freetype_load_target = "Normal",
   line_height = 1.4,
   text_blink_rate = 500,
-  cell_width = 1,
-  font = wezterm.font("Sometype Mono"),
+  cell_width = 0.9,
+  font = wezterm.font("Office Code Pro"),
 }
