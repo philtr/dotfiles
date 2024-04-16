594f5fc74d5ced14cafb535b65da18b17c5df686
Phillip Ridlen <p@rdln.net>
2024-04-15 21:24:28 -0500

    Li'l bigger font for my aging eyeballs

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -4,10 +4,10 @@ return {
 	font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
 
 	-- cell_width = 0.9,
-	font_size = 12,
+	font_size = 14,
 	-- freetype_load_flags = "DEFAULT",
 	-- freetype_load_target = "Normal",
 	-- freetype_render_target = "Normal",
-	line_height = 1.4,
+	line_height = 1.2,
 	text_blink_rate = 500,
 }
