45d47b1c5251bd61eac45a3f97ec09098c3b0f02
Phillip Ridlen <p@rdln.net>
2024-05-31 15:29:28 -0500

    Adjust wezterm window size and padding

--- .config/wezterm/window.lua
+++ .config/wezterm/window.lua
@@ -8,11 +8,14 @@ return {
 	-- allow window to be moved and resized but do not show title bar
 	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
 
+	initial_rows = 32,
+	initial_cols = 128,
+
 	-- units are in pixels unless otherwise specified
 	window_padding = {
 		top = 60,
-		right = 0,
-		bottom = 0,
-		left = 0,
+		right = 15,
+		bottom = 15,
+		left = 15,
 	},
 }
