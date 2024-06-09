b8adef1a6e8d109da5250cbd6c288aa2bb5d3bdb
Phillip Ridlen <p@rdln.net>
2024-06-09 16:44:14 -0500

    Add test coverage view to gutter

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -3,4 +3,9 @@ return {
     "jfpedroza/neotest-elixir",
     branch = "master",
   },
+  {
+    "andythigpen/nvim-coverage",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
 }
