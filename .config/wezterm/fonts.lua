9e61006a7ef9c1b5a5ff04432c2c9c48cf09a658
Phillip Ridlen <p@rdln.net>
2024-06-09 16:43:49 -0500

    Use Monaspace

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
+	font = wezterm.font("Monaspace Neon", { weight = "Medium" }),
 
 	-- cell_width = 0.9,
 	font_size = 14,
 	-- freetype_load_flags = "DEFAULT",
 	-- freetype_load_target = "Normal",
 	-- freetype_render_target = "Normal",
-	line_height = 1.2,
+	line_height = 1.3,
 	text_blink_rate = 500,
 }
