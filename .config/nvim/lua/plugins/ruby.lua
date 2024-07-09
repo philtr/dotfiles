0152cf05e97362495e7d969ec1648211f7f26a41
Phillip Ridlen <p@rdln.net>
2024-07-09 16:33:44 -0500

    Use ruby-lsp instead of solargraph

--- .config/nvim/lua/plugins/ruby.lua
+++ .config/nvim/lua/plugins/ruby.lua
@@ -1,22 +1,16 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
+      ---@type lspconfig.options
       servers = {
+        -- disable solargraph from auto running when you open ruby files
         solargraph = {
-          cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
-          settings = {
-            solargraph = {
-              autoformat = true,
-              completion = true,
-              diagnostics = true,
-              folding = true,
-              references = true,
-              rename = true,
-              symbols = true,
-            },
-          },
+          autostart = false,
         },
+        -- ruby_lsp will be automatically installed with mason and loaded with lspconfig
+        ruby_lsp = {},
       },
     },
   },
