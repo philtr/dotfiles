912ca97a942c56cfd3ed9ba2f83aa85f384b5703
Phillip Ridlen <p@rdln.net>
2025-10-08 09:44:27 -0500

    feat(wezterm): Restore default Alt key behavior to get chars like é™ü

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,6 +1,13 @@
 local act = require("wezterm").action
 
 return {
+	-- The default behavior is to treat the left Option key as the Alt modifier
+	-- with no composition effects, while the right Option key performs composition
+	-- (making it approximately equivalent to AltGr on other operating systems).
+	-- This configuration restores the default macOS behavior.
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = true,
+
 	-- use physical keybinding (because Dvorak - <cmd>QWERTY)
 	-- NOTE: this seems backwards to me, but this is what works 🤷
 	key_map_preference = "Physical",
