a33575c8ac7783de875f47e682d4510565554a3d
Phillip Ridlen <p@rdln.net>
2026-01-17 10:04:28 -0600

    feat(nvim, wezterm): use nvim default colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,61 +3,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "storm",
-      transparent = true,
-      terminal_colors = true,
-      -- day_brightness = 0.7,
-      on_colors = function(colors) -- change some colors
-        colors.bg = "#ffffff"
-      end,
-    },
-  },
-  {
-    "shaunsingh/nord.nvim",
-    config = function()
-      vim.g.nord_bold = true
-      vim.g.nord_italic = true
-
-      -- Uncomment to properly set Nord theme
-      -- require("nord").set()
-    end,
-  },
-  {
-    "Everblush/nvim",
-    name = "everblush",
-
-    opts = {
-      transparent_background = true,
-      override = {
-        TelescopeSelection = { bg = "TelescopeBorder" },
-        NotifyBackground = { bg = "Background" },
-      },
-    },
-  },
-  {
-    "navarasu/onedark.nvim",
-    opts = {
-      style = "light",
-      lualine = { transparent = true },
-      code_style = {
-        keywords = "italic",
-        strings = "italic",
-      },
-      diagnostics = {
-        darker = false,
-      },
-    },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      contrast = "hard",
+      colorscheme = "default",
     },
   },
 }
